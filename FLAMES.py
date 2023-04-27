98173c2d2a1c58b7b67a31d418e5ae8006a01fc5
surendar <surendar@surendars-iMac.local>
2023-04-27 09:27:32 +0530

    FLAMES

--- FLAMES.py
+++ FLAMES.py
@@ -10,14 +10,25 @@ for x in range(len(player1)):
 player_concated = player1 + player2
 player_concated = player_concated.replace(' ', '')
 
-flames = ['F','L','A','M','E','S']
+flames = 'flames'
 
-def flames_appender(player_concated, flames):
-	to_add = len(player_concated) - len(flames)
-	for i in range(to_add):
-		flames.append(flames[i])
+def flames_appender(player_concated):
+	global flames
+	for i in range(len(player_concated)+2):
+		flames = flames + 'flames'
 
-if len(player_concated) > len(flames):
-	flames_appender(player_concated, flames)
+flames_appender(player_concated)
+
+to_replace = flames[4::len(player_concated)]
+print(to_replace)
+
+for z in range(len(to_replace)):
+	flames = flames.replace(to_replace[z], '')
 
 print(flames)
+
+flames_describe = {'f': 'Friend', 'l': 'Lover', 'a': 'Affectionate', 'm': 'Marriage', 'e': 'Enemies', 's': 'Sibling'}
+
+for key, value in flames_describe.items():
+	if key == flames[0]:
+		print(value)
